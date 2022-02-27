7841fa9efa04f82be9a1bc86c7410b319ec92e24
heimesh <heimesh79@gmail.com>
2022-02-27 21:56:18 +0530

    ability to draw added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const defaultSize = 16;
+let colorPicked
 
 let grid = document.querySelector('.grid');
+let erase = document.querySelector('.erase');
+let colorBtn = document.querySelector('.colorBtn');
+let clear = document.querySelector('.clear');
+let color = document.querySelector('#color');
+
 
 /*Creates a grid */
 function makeGrid(size) {
@@ -9,11 +15,44 @@ function makeGrid(size) {
   
     for (let i = 0; i < size * size; i++) {
       const gridElement = document.createElement('div');
-      gridElement.classList.add("col")
-      grid.appendChild(gridElement)
+      gridElement.classList.add("col");
+      grid.appendChild(gridElement);
     }
 }
 
-window.onload = () => {
+/*window.onload = () => {
     makeGrid(defaultSize);
-  }
+}*/
+
+makeGrid(16);
+
+const gridBox = document.querySelectorAll('.col');
+
+color.addEventListener('input', () => {
+  colorPicked = color.value;
+});
+
+colorBtn.addEventListener('click', function(){
+  gridBox.forEach( gridBox => {
+    gridBox.addEventListener("mousedown", e =>{
+      e.target.style.backgroundColor = "colorPicked";
+    });
+  });
+});
+
+erase.addEventListener("click", function(){
+  gridBox.forEach( gridBox => {
+    gridBox.addEventListener("click", e =>{
+      e.target.style.backgroundColor = "white";
+    });
+  });
+});
+
+clear.addEventListener("click", function(){
+  gridBox.forEach( gridBox => {
+    gridBox.style.backgroundColor = "white";
+  });
+});
+
+
+
